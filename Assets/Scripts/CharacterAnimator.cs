77e5e5e91982437362ba49bae285de9edb34f396
nicholaschuayunzhi <nicholaschuayunzhi@gmail.com>
2019-05-11 19:17:01 +0800

    add animations * Updated player prefab with animators

---
+++ Assets/Scripts/CharacterAnimator.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Animator))]
+public class CharacterAnimator : MonoBehaviour
+{
+    Animator animator;
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    public void Move(bool isMoving = true)
+    {
+        animator.SetBool("Moving", isMoving);
+    }
+
+    public void Jump()
+    {
+        animator.SetTrigger("Jump");
+    }
+
+    public void Ground(bool isGrounded = true)
+    {
+        animator.SetBool("Grounded", isGrounded);
+    }
+
+    public void Hurt()
+    {
+        animator.SetTrigger("Hurt");
+    }
+}
