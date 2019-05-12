7a915e2119f4f14295fc17b51c00e590362e48a1
sharkcircuits <47176493+sharkcircuits@users.noreply.github.com>
2019-05-12 16:05:43 +0800

    Added Basic GameManager functionality
    Added a GameManager object that does the following:
    - Keeps track of statistics such as the player's score and the time elapsed
    - Keeps track of whether the player is dead
    - Updates the time elapsed every frame, and communicates with other time-reliant objects
    - Handles the UI for score and time, and eventually the Game Over screen once player death is implemented

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,51 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+    int score = 0;
+    int enemiesDefeated = 0;
+    float timeSinceGameStart = 0.0f;
+    bool playerDead = false;
+
+    public Text scoreDisplay;
+    public Text timeDisplay;
+    public Text endgameDisplay;
+
+    void Awake()
+    {
+        endgameDisplay.enabled = false;
+    }
+
+    void Update ()
+    {
+        float timeElapsedFromPreviousFrame = Time.deltaTime;
+        timeSinceGameStart += timeElapsedFromPreviousFrame;
+        sendSignalToSpawnPoints(timeElapsedFromPreviousFrame);
+
+        scoreDisplay.text = score.ToString();
+        timeDisplay.text = timeSinceGameStart.ToString();
+    }
+
+    void sendSignalToSpawnPoints(float timeElapsedFromPreviousFrame)
+    {
+        /*
+         * I think it makes more sense for each spawner to be in-charge of its own spawning.
+         * The GameManager will track how much time has progressed, and send this
+         * info to the spawners. Whenever enough time has elapsed in each spawner,
+         * it will shoot out the next enemy and reset the time that has passed.
+         */
+    }
+
+    void enemyDies()
+    {
+        score++;
+        enemiesDefeated++;
+    }
+
+    void playerCharacterDies()
+    {
+        playerDead = true;
+        endgameDisplay.enabled = true;
+    }
+}
