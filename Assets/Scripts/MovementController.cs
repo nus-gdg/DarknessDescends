a731d08da9856a12f3972d674b5b68aca71f1ad6
Yeo Tong <helloimyeotong@gmail.com>
2019-05-17 01:24:07 +0800

    Added PlayerMovement and MovemnetController script

---
+++ Assets/Scripts/MovementController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class MovementController : MonoBehaviour
+{
+    private Rigidbody2D rb;
+    private float gravity;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        gravity = Physics2D.gravity.y;
+    }
+
+    public void Move(float movementSpeed, bool isGrounded = true)
+    {
+        float horizontalVelocity = Input.GetAxis("Horizontal") * movementSpeed;
+        rb.velocity = new Vector2 (rb.velocity.x, rb.velocity.y + gravity * Time.deltaTime);
+        float verticalVelocity = rb.velocity.y;
+        if (isGrounded)
+        {
+            verticalVelocity = 0.0f;
+        }
+        Vector3 movement = new Vector3(horizontalVelocity, verticalVelocity, 0.0f) * Time.deltaTime;
+        rb.MovePosition(transform.position + movement);
+    }
+
+    public void Jump(float jumpForce)
+    {
+        rb.AddForce(new Vector2(0.0f, 2.0f) * jumpForce, ForceMode2D.Impulse);
+    }
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D), typeof(MovementController))]
+public class PlayerMovement : MonoBehaviour
+{
+    private MovementController movementController;
+    private CharacterAnimator charAnimator;
+
+    private bool isGrounded;
+    private int x;
+
+    [SerializeField]
+    public float movementSpeed;
+
+    [SerializeField]
+    public float jumpForce;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        movementController = GetComponent<MovementController>();
+        charAnimator = GetComponent<CharacterAnimator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.RightArrow))
+        {
+            movementController.Move(movementSpeed, isGrounded);
+            if (isGrounded)
+            {
+                charAnimator.Move();
+            }
+        }
+
+        else if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
+        {
+            movementController.Jump(jumpForce);
+            charAnimator.Jump();
+            isGrounded = false;
+        }
+
+        else
+        {
+            charAnimator.Move(false);
+        }
+
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Ground")
+        {
+            if (!isGrounded)
+            {
+                charAnimator.Ground();
+            }
+            isGrounded = true;
+        }
+    }
+}
