c985c2cb26328d57069d86997c11d5484edafc66
nicholaschuayunzhi <nicholaschuayunzhi@gmail.com>
2019-06-09 16:13:57 +0800

    Added loot despawning

--- Assets/Scripts/Loot.cs
+++ Assets/Scripts/Loot.cs
@@ -7,12 +7,20 @@ using UnityEngine.Assertions;
 [RequireComponent(typeof(BoxCollider2D))]
 public class Loot : MonoBehaviour
 {
+    public float DespawnTime = 5;
     [HideInInspector]
     public Weapon WeaponDrop; // assume we have a weapon as a child game object
     void Start()
     {
         WeaponDrop = GetComponentInChildren<Weapon>();
         Assert.IsTrue(WeaponDrop != null);
+        StartCoroutine(DespawnInSeconds(5));
+    }
+
+    IEnumerator DespawnInSeconds(float seconds)
+    {
+        yield return new WaitForSeconds(seconds);
+        Destroy(gameObject);
     }
 
     void OnTriggerEnter2D(Collider2D other)
