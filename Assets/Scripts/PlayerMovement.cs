78d440b5de3dcffb0f062f2912851ec1b585cc40
Wingedevil <jerroldtan@gmail.com>
2019-08-03 16:03:13 +0800

    fix: Fixed hitboxes and jump animation bug
    Cleaned up jump and hitboxes
    Fixed being stuck in jump animation when hit by enemy

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -3,8 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [RequireComponent(typeof(Rigidbody2D), typeof(MovementController))]
-public class PlayerMovement : MonoBehaviour
-{
+public class PlayerMovement : MonoBehaviour {
     private MovementController movementController;
     private CharacterAnimator charAnimator;
 
@@ -19,41 +18,29 @@ public class PlayerMovement : MonoBehaviour
     public float jumpForce;
 
     // Start is called before the first frame update
-    private void Start()
-    {
+    private void Start() {
         movementController = GetComponent<MovementController>();
         charAnimator = GetComponent<CharacterAnimator>();
     }
 
     // Update is called once per frame
-    private void Update()
-    {
-        if (Input.GetAxisRaw("Vertical") > 0 && isGrounded)
-        {
+    private void Update() {
+        if (Input.GetAxisRaw("Vertical") > 0 && isGrounded) {
             isJumping = true;
         }
 
-        if (Input.GetAxisRaw("Horizontal") > 0)
-        {
+        if (Input.GetAxisRaw("Horizontal") > 0) {
             isMoving = 1;
-        }
-
-        else if (Input.GetAxisRaw("Horizontal") < 0)
-        {
+        } else if (Input.GetAxisRaw("Horizontal") < 0) {
             isMoving = -1;
-        }
-
-        else
-        {
+        } else {
             isMoving = 0;
         }
     }
 
 
-    private void FixedUpdate()
-    {
-        if (isJumping)
-        {
+    private void FixedUpdate() {
+        if (isJumping) {
             movementController.Jump(jumpForce);
             charAnimator.Jump();
             charAnimator.Move(false);
@@ -62,40 +49,38 @@ public class PlayerMovement : MonoBehaviour
             isJumping = false;
         }
 
-        if (isMoving < 0)
-        {
+        if (isMoving < 0) {
             movementController.MoveLeft(movementSpeed);
             charAnimator.faceLeft();
-            if (isGrounded)
-            {
+            if (isGrounded) {
                 charAnimator.Move();
             }
-        }
-
-        else if (isMoving > 0)
-        {
+        } else if (isMoving > 0) {
             movementController.MoveRight(movementSpeed);
             charAnimator.faceRight();
-            if (isGrounded)
-            {
+            if (isGrounded) {
                 charAnimator.Move();
             }
-        }
-
-        else
-        {
+        } else {
             movementController.Stationary();
             charAnimator.Move(false);
         }
 
     }
 
-    private void OnCollisionEnter2D(Collision2D collision)
-    {
-        if (collision.gameObject.tag == "Ground")
-        {
-            if (!isGrounded)
-            {
+    private void OnCollisionEnter2D(Collision2D collision) {
+        Vector2[] rays = { new Vector2(0f, 0f), new Vector2(0.5f, 0f), new Vector2(-0.5f, 0f), new Vector2(0.25f, 0f), new Vector2(-0.25f, 0f) };
+        int hits = 0;
+        foreach (Vector2 displace in rays) {
+            RaycastHit2D[] hitArray = new RaycastHit2D[3];
+            GetComponent<Collider2D>().Raycast(new Vector2(0f, -1f), hitArray, 1.3f, LayerMask.NameToLayer("Player"));
+            if (hitArray[0].collider != null && hitArray[0].collider.tag == "Ground") {
+                hits++;
+            }
+        }
+        Debug.Log(hits);
+        if (collision.gameObject.tag == "Ground" && hits > rays.Length / 2) {
+            if (!isGrounded) {
                 charAnimator.Ground();
             }
             isGrounded = true;
--- Assets/Scripts/WeaponController.cs
+++ Assets/Scripts/WeaponController.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-[RequireComponent(typeof(BoxCollider2D))]
+[RequireComponent(typeof(Collider2D))]
 public class WeaponController : MonoBehaviour
 {
     public Transform MeleeWeaponTransform;
